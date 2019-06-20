0d6cfc8b890c47dc4e083c79b14e04a8e6a19833
M Banks <montensia1@outlook.com>
2019-06-20 15:46:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ class Dog
   
   def self.find_by_id(id_num)
     sql = <<-SQL
-      SELECT * FROM dogs WHERE id = ?
+      SELECT * FROM dogs WHERE id = ? LIMIT 1
     SQL
     DB[:conn].execute(sql, id_num).map do |row|
       self.new_from_db(row)
